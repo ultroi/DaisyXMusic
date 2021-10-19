3be8cb7b35197d71151568b38dfb2838d0620db5
ultroi <86416271+ultroi@users.noreply.github.com>
2021-10-19 06:23:37 +0530

    Update play.py

--- DaisyXMusic/modules/play.py
+++ DaisyXMusic/modules/play.py
@@ -979,7 +979,7 @@ async def jiosaavn(client: Client, message_: Message):
                     # print(e)
                     await lel.edit(
                         f"<b>🔴 Flood Wait Error 🔴 \nUser {user.first_name} couldn't join your group due to heavy requests for userbot! Make sure user is not banned in group."
-                        "\n\nOr manually add @DaisyXmusic to your Group and try again</b>",
+                        "\n\nOr manually add {BOT_USERNAME} to your Group and try again</b>",
                     )
     try:
         await USER.get_chat(chid)
